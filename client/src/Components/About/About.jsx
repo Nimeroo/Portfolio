10c3354ca3f99a9b7f8a33577074189a24256371
Nimeroo <girou.joe@gmail.com>
2022-01-25 18:59:57 -0500

    dom layout and and information added to about component

---
+++ client/src/Components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+      <div id="about-cont">
+        <img id="round-img"></img>
+        <div id="about-info">
+          <h1>Software Developer</h1>
+          <h4>
+            I am an aspiring Software Developer who has an immense amount of
+            interest in the future of technology. I address issues through the
+            use of creative and logical thinking. My innate ability to adapt
+            allows me to become accustomed to new situations and environments
+            extremely fast.
+          </h4>
+        </div>
+      </div>
+  );
+};
+
+export default About;
