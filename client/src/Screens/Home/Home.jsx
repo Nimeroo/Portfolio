d28dffbcc182b1a9bd7bed43aa77fda6a1bbcd50
Nimeroo <girou.joe@gmail.com>
2022-01-26 05:03:44 -0500

    Contact component imported to home screen

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../../Components/About/About";
 import Hero from "../../Components/Hero/Hero";
 import Skills from "../../Components/Skills/Skills";
+import Contact from "../../Components/Contact/Contact";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <About />
       <Skills />
+      <Contact />
     </div>
   );
 };
