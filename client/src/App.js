4a9e7ed3cb0cba83295d497382e54d274a2ea8e3
Nimeroo <girou.joe@gmail.com>
2022-01-26 02:34:01 -0500

    Home screen imported to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Nav from "./Components/Nav/Nav";
+import Home from "./Screens/Home/Home";
 
 function App() {
   return <div className="App">
     <Nav />
-    
-
+    <Home />
   </div>;
 }
 
