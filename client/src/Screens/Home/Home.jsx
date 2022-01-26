8f24360c8635bd17b187c3aab1c773153dc54fdb
Nimeroo <girou.joe@gmail.com>
2022-01-26 02:57:30 -0500

    About, Hero, and Skills components imported to home screen

---
+++ client/src/Screens/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "../../Components/About/About";
+import Hero from "../../Components/Hero/Hero";
+import Skills from "../../Components/Skills/Skills";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Skills />
+    </div>
+  );
+};
+
+export default Home;
