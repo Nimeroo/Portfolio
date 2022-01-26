1ca64c13d135ab115119baf17f60631124cc8a94
Nimeroo <girou.joe@gmail.com>
2022-01-26 07:34:52 -0500

    about link added to nav and links for project screen and home screen added

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div>
-      <h1>Joel Giroux</h1>
-      <h3>Home</h3>
-      <h3>Projects</h3>
-      <h3>Contact</h3>
+      <Link to="/">
+        <h1>Joel Giroux</h1>
+      </Link>
+      <div id="nav-links">
+        <Link to="/">
+          <h3>Home</h3>
+        </Link>
+        <h3>About</h3>
+        <Link to="/projects">
+          <h3>Projects</h3>
+        </Link>
+        <h3>Contact</h3>
+      </div>
     </div>
   );
 };
