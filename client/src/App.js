fe0689ff8595b0ddbe9783752486d389a311ac2b
Nimeroo <girou.joe@gmail.com>
2022-01-26 07:32:42 -0500

    projects screen imported to app and routes for home and projects setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Screens/Home/Home";
+import Projects from "./Screens/Projects/Projects";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  return <div className="App">
-    <Nav />
-    <Home />
-  </div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
