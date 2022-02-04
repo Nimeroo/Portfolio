4cfbaa683cdf33b305dc400a43b3512ac3fe1967
Nimeroo <girou.joe@gmail.com>
2022-02-04 03:30:17 -0500

    about component css design implemented

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,12 +1,15 @@
+import "./About.css";
 import React from "react";
 
 const About = () => {
   return (
+    <div id="about">
+      <h1 id="about-title">About me</h1>
       <div id="about-cont">
         <img id="round-img"></img>
         <div id="about-info">
           <h1>Software Developer</h1>
-          <h4>
+          <h4 id="about-text">
             I am an aspiring Software Developer who has an immense amount of
             interest in the future of technology. I address issues through the
             use of creative and logical thinking. My innate ability to adapt
@@ -15,6 +18,7 @@ const About = () => {
           </h4>
         </div>
       </div>
+    </div>
   );
 };
 
