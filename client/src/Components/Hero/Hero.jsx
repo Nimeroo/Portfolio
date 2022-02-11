14228f6e1ac3424c1e051bc91bcb5ac47ee4aeb2
Nimeroo <girou.joe@gmail.com>
2022-02-11 15:15:20 -0500

    content and animations added to hero

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,10 +1,25 @@
-import "./Hero.css"
+import "./Hero.css";
 import React from "react";
 
 const Hero = () => {
   return (
-      <div id="animated-hero"></div>
+    <div id="hero-container">
+      <div id="hero-wrapper">
+        <div id="hero-name">
+          <h1>Joel</h1>
+          <h1>Giroux</h1>
+        </div>
+        <div id="hovered-hero-name">
+          <h1>Joel</h1>
+          <h1>Giroux</h1>
+        </div>
+      </div>
+      <div id="secondary-text">
+        <h3>Frontend Developer</h3>
+        <h3>Backend Developer</h3>
+      </div>
+    </div>
   );
 };
 
-export default Hero
+export default Hero;
