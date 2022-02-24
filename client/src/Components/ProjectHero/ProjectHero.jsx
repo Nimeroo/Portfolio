5b820832c125a781c2413986fa2ff66e1f62ab62
Nimeroo <girou.joe@gmail.com>
2022-02-23 21:48:44 -0500

    project hero component created

---
+++ client/src/Components/ProjectHero/ProjectHero.jsx
@@ -0,0 +1,17 @@
+import "./ProjectHero.css"
+import React from "react";
+
+export const ProjectHero = () => {
+  return (
+    <div id="project-hero">
+      <div class="project-hero-title project-hero-title--initial">
+        <h1>Projects</h1>
+      </div>
+      <div class="project-hero-title project-hero-title--hovered">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectHero;
