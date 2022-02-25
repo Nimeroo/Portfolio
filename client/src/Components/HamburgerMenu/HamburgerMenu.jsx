8f33eb5d5899bcd3e09beace5c45e954fdcdd822
Nimeroo <girou.joe@gmail.com>
2022-02-25 09:10:05 -0500

    hamburger menu component created and implemented

---
+++ client/src/Components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,69 @@
+import "./HamburgerMenu.css";
+import { MenuItem, MenuList, Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import react from "react";
+
+const HamburgerMenu = ({
+  updateHamburger,
+  scrollTo,
+  homeReference,
+  aboutReference,
+  skillsReference,
+  contactReference,
+}) => {
+  return (
+    <Paper elevation={10} id="hamburger-nav-links-cont">
+      <MenuList>
+        <Link
+          to="/"
+          onClick={() => {
+            scrollTo(homeReference);
+            updateHamburger();
+          }}
+          className="hamburger-nav-link"
+        >
+          <MenuItem>Home</MenuItem>
+        </Link>
+        <Link
+          to="/"
+          onClick={() => {
+            scrollTo(aboutReference);
+            updateHamburger();
+          }}
+          className="hamburger-nav-link"
+        >
+          <MenuItem>About</MenuItem>
+        </Link>
+        <Link
+          to="/"
+          onClick={() => {
+            scrollTo(skillsReference);
+            updateHamburger();
+          }}
+          className="hamburger-nav-link"
+        >
+          <MenuItem>Skills</MenuItem>
+        </Link>
+        <Link
+          to="/projects"
+          onClick={() => updateHamburger()}
+          className="hamburger-nav-link"
+        >
+          <MenuItem>Projects</MenuItem>
+        </Link>
+        <Link
+          to="/"
+          onClick={() => {
+            scrollTo(contactReference);
+            updateHamburger();
+          }}
+          className="hamburger-nav-link"
+        >
+          <MenuItem>Contact</MenuItem>
+        </Link>
+      </MenuList>
+    </Paper>
+  );
+};
+
+export default HamburgerMenu;
