592f74b2220fbd3e8083a7a35ceb9d19aa42b2d9
Nimeroo <girou.joe@gmail.com>
2022-02-25 09:09:20 -0500

    conditional added to change navlinks to hamburger menu based on screen width

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,7 +1,11 @@
 import "./Nav.css";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import React, { useState } from "react";
 import { Toolbar } from "@mui/material";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
+import mainLogo from "../../Assets/main-logo.png";
 
 const Nav = ({
   scrollTo,
@@ -11,6 +15,7 @@ const Nav = ({
   contactReference,
 }) => {
   const [boxShadower, setBoxShadower] = useState(0);
+  const [hamburgerStatus, setHamburgerStatus] = useState(false);
 
   window.onscroll = () => {
     if (window.pageYOffset > 20) {
@@ -20,27 +25,55 @@ const Nav = ({
     }
   };
 
+  const updateHamburger = () => {
+    if (hamburgerStatus === false) {
+      setHamburgerStatus(true);
+    } else setHamburgerStatus(false);
+  };
+
+  const navlinks = (
+    <>
+      <Link to="/" onClick={() => scrollTo(homeReference)}>
+        <h3 className="nav-link">Home</h3>
+      </Link>
+      <Link to="/" onClick={() => scrollTo(aboutReference)}>
+        <h3 className="nav-link">About</h3>
+      </Link>
+      <Link to="/" onClick={() => scrollTo(skillsReference)}>
+        <h3 className="nav-link">Skills</h3>
+      </Link>
+      <Link to="/projects">
+        <h3 className="nav-link">Projects</h3>
+      </Link>
+      <Link to="/" onClick={() => scrollTo(contactReference)}>
+        <h3 className="nav-link">Contact</h3>
+      </Link>
+    </>
+  );
+
   return (
     <Toolbar sx={{ boxShadow: boxShadower }} variant="dense" id="navbar">
       <Link to="/">
-        <h1 id="logo">Joel Giroux</h1>
+        <img id="logo" src={mainLogo}></img>
       </Link>
       <div id="nav-links-cont">
-        <Link to="/" onClick={() => scrollTo(homeReference)}>
-          <h3 className="nav-link">Home</h3>
-        </Link>
-        <Link to="/" onClick={() => scrollTo(aboutReference)}>
-          <h3 className="nav-link">About</h3>
-        </Link>
-        <Link to="/" onClick={() => scrollTo(skillsReference)}>
-          <h3 className="nav-link">Skills</h3>
-        </Link>
-        <Link to="/projects">
-          <h3 className="nav-link">Projects</h3>
-        </Link>
-        <Link to="/" onClick={() => scrollTo(contactReference)}>
-          <h3 className="nav-link">Contact</h3>
-        </Link>
+        {window.screen.width <= 600 ? (
+          <IconButton onClick={() => updateHamburger()}>
+            <MenuIcon />
+          </IconButton>
+        ) : (
+          navlinks
+        )}
+        {hamburgerStatus && window.screen.width <= 600 ? (
+          <HamburgerMenu
+            updateHamburger={updateHamburger}
+            scrollTo={scrollTo}
+            homeReference={homeReference}
+            aboutReference={aboutReference}
+            skillsReference={skillsReference}
+            contactReference={contactReference}
+          />
+        ) : null}
       </div>
     </Toolbar>
   );
