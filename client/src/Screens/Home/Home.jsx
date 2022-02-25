01cf5ebfce31881d6b9c761be902f2f81aad0fdd
Nimeroo <girou.joe@gmail.com>
2022-02-25 09:12:14 -0500

    home screen code reformatted

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -4,13 +4,19 @@ import Hero from "../../Components/Hero/Hero";
 import Skills from "../../Components/Skills/Skills";
 import Contact from "../../Components/Contact/Contact";
 
-const Home = ({scrollTo, homeReference, aboutReference, skillsReference, contactReference}) => {
+const Home = ({
+  scrollTo,
+  homeReference,
+  aboutReference,
+  skillsReference,
+  contactReference,
+}) => {
   return (
     <div>
-      <Hero homeReference={homeReference}/>
-      <About aboutReference={aboutReference}/>
-      <Skills scrollTo={scrollTo} skillsReference={skillsReference}/>
-      <Contact contactReference={contactReference}/>
+      <Hero homeReference={homeReference} />
+      <About aboutReference={aboutReference} />
+      <Skills scrollTo={scrollTo} skillsReference={skillsReference} />
+      <Contact contactReference={contactReference} />
     </div>
   );
 };
